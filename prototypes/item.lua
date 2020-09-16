a558cc09fb3d680b145ff2d9c24a9f42818fb41a
Asta Zora <Tas_King@outlook.com>
2020-09-16 18:08:12 -0400

    Creation of the mod

---
+++ prototypes/item.lua
@@ -0,0 +1,59 @@
+data:extend(
+{
+type = "item",
+name = "monazite-ore",
+icon = "__SAM_2__/graphics/icons/Monazite-Ore.png",
+icon_size = 64, icon_mipmaps = 4,
+pictures = 
+ {
+ { size = 64, filename = "__SAM_2__/graphics/icons/monazite-ore",  scale = 0.25, mipmap_count = 4}
+ },
+ subgroup = "raw_resources",
+ order = "a[monazite-ore]",
+ stacksize = 50
+},
+
+{
+type = "item",
+name = "thorium-ore",
+icon = "__SAM_2__/graphics/icons/thorium-ore.png",
+icon_size = 64, icon_mipmaps = 4,
+pictures = 
+ {
+ { size = 64, filename = "__SAM_2__/graphics/icons/thorium-ore",  scale = 0.25, mipmap_count = 4}
+ },
+ subgroup = "raw_resources",
+ order = "b[thorium-ore]",
+ stacksize = 50
+},
+
+{
+type = "item",
+name = "samarium-ore",
+icon = "__SAM_2__/graphics/icons/samarium-ore.png",
+icon_size = 64, icon_mipmaps = 4,
+pictures = 
+     {
+     { size = 64, filename = "__SAM_2__/graphics/icons/samarium-ore",  scale = 0.25, mipmap_count = 4}
+     },
+subgroup = "raw_resources",
+order = "c[samarium-ore]",
+stacksize = 50
+},
+
+{
+type = "item",
+name = "uranium-233",
+icon = "__SAM_2.0__/graphics/icons/uranium-233.png",
+icon_size = 64, icon_mipmaps = 4,
+pictures = 
+ {
+ { size = 64, filename = "__SAM_2.0__/graphics/icons/uranium-233",  scale = 0.25, mipmap_count = 4}
+ },
+ subgroup = "raw_resources",
+ order = "d[uranium-233]",
+ stacksize = 50
+
+}
+)
+
