b9182dfe9692969fdbef75350d37b98ece24c8fb
Samuel Gagnon <gagnonsamu@gmail.com>
2020-09-16 21:20:07 -0400

    Fix item.lua and add placeholder images

---
+++ prototypes/item/item.lua
@@ -0,0 +1,58 @@
+data:extend(
+{
+  {
+    type = "item",
+    name = "monazite-ore",
+    icon = "__Sky-Atlas-Metallurgy-Rebirth__/graphics/icons/monazite-ore.png",
+    icon_size = 64, icon_mipmaps = 4,
+    pictures =
+    {
+      { size = 64, filename = "__Sky-Atlas-Metallurgy-Rebirth__/graphics/icons/monazite-ore.png",  scale = 0.25, mipmap_count = 4}
+    },
+    subgroup = "raw-resource",
+    order = "a[monazite-ore]",
+    stack_size = 50
+  },
+
+  {
+    type = "item",
+    name = "thorium-ore",
+    icon = "__Sky-Atlas-Metallurgy-Rebirth__/graphics/icons/thorium-ore.png",
+    icon_size = 64, icon_mipmaps = 4,
+    pictures =
+    {
+      { size = 64, filename = "__Sky-Atlas-Metallurgy-Rebirth__/graphics/icons/thorium-ore.png",  scale = 0.25, mipmap_count = 4}
+    },
+    subgroup = "raw-resource",
+    order = "b[thorium-ore]",
+    stack_size = 50
+  },
+
+  {
+    type = "item",
+    name = "samarium-ore",
+    icon = "__Sky-Atlas-Metallurgy-Rebirth__/graphics/icons/samarium-ore.png",
+    icon_size = 64, icon_mipmaps = 4,
+    pictures =
+    {
+      { size = 64, filename = "__Sky-Atlas-Metallurgy-Rebirth__/graphics/icons/samarium-ore.png",  scale = 0.25, mipmap_count = 4}
+    },
+    subgroup = "raw-resource",
+    order = "c[samarium-ore]",
+    stack_size = 50
+  },
+
+  {
+    type = "item",
+    name = "uranium-233",
+    icon = "__Sky-Atlas-Metallurgy-Rebirth__/graphics/icons/uranium-233.png",
+    icon_size = 64, icon_mipmaps = 4,
+    pictures =
+    {
+      { size = 64, filename = "__Sky-Atlas-Metallurgy-Rebirth__/graphics/icons/uranium-233.png",  scale = 0.25, mipmap_count = 4}
+    },
+    subgroup = "raw-resource",
+    order = "d[uranium-233]",
+    stack_size = 50
+  }
+})
