5068a7c848a8479eeb8f4cc4cae43c91565bd0a6
Samuel Gagnon <gagnonsamu@gmail.com>
2020-09-20 18:38:45 -0400

    Remove iron and copper

--- data.lua
+++ data.lua
@@ -1,1 +1,18 @@
 require("prototypes.item.item")
+
+local function removeResource(resource)
+  -- Remove from presets
+  for _, preset in pairs(data.raw["map-gen-presets"]["default"]) do
+    if
+    preset and preset.basic_settings and preset.basic_settings.autoplace_controls and
+      preset.basic_settings.autoplace_controls[resource]
+    then
+      data.raw["resource"][resource] = nil
+      data.raw["autoplace-control"][resource] = nil
+      preset.basic_settings.autoplace_controls[resource] = nil
+    end
+  end
+end
+
+removeResource("iron-ore")
+removeResource("copper-ore")
