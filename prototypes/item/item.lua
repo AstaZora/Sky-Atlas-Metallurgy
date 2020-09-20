86e810f6f643993038be99e2d0911ac96476d3af
Samuel Gagnon <gagnonsamu@gmail.com>
2020-09-20 19:41:17 -0400

    Adding the other ores of Sky Atlas Metallurgy

--- prototypes/item/item.lua
+++ prototypes/item/item.lua
@@ -2,6 +2,38 @@ data:extend(
 {
   {
     type = "item",
+    name = "magnetite-ore",
+    icon = "__Sky-Atlas-Metallurgy-Rebirth__/graphics/icons/magnetite-ore.png",
+    icon_size = 64, icon_mipmaps = 4,
+    pictures =
+    {
+      { size = 64, filename = "__Sky-Atlas-Metallurgy-Rebirth__/graphics/icons/magnetite-ore.png",   scale = 0.25, mipmap_count = 4 },
+      { size = 64, filename = "__Sky-Atlas-Metallurgy-Rebirth__/graphics/icons/magnetite-ore-1.png", scale = 0.25, mipmap_count = 4 },
+      { size = 64, filename = "__Sky-Atlas-Metallurgy-Rebirth__/graphics/icons/magnetite-ore-2.png", scale = 0.25, mipmap_count = 4 },
+      { size = 64, filename = "__Sky-Atlas-Metallurgy-Rebirth__/graphics/icons/magnetite-ore-3.png", scale = 0.25, mipmap_count = 4 }
+    },
+    subgroup = "raw-resource",
+    order = "i[magnetite-ore]", -- g[uranium-ore], h[raw-fish]
+    stack_size = 50
+  },
+  {
+    type = "item",
+    name = "malachite-ore",
+    icon = "__Sky-Atlas-Metallurgy-Rebirth__/graphics/icons/malachite-ore.png",
+    icon_size = 64, icon_mipmaps = 4,
+    pictures =
+    {
+      { size = 64, filename = "__Sky-Atlas-Metallurgy-Rebirth__/graphics/icons/malachite-ore.png",   scale = 0.25, mipmap_count = 4 },
+      { size = 64, filename = "__Sky-Atlas-Metallurgy-Rebirth__/graphics/icons/malachite-ore-1.png", scale = 0.25, mipmap_count = 4 },
+      { size = 64, filename = "__Sky-Atlas-Metallurgy-Rebirth__/graphics/icons/malachite-ore-2.png", scale = 0.25, mipmap_count = 4 },
+      { size = 64, filename = "__Sky-Atlas-Metallurgy-Rebirth__/graphics/icons/malachite-ore-3.png", scale = 0.25, mipmap_count = 4 }
+    },
+    subgroup = "raw-resource",
+    order = "j[malachite-ore]",
+    stack_size = 50
+  },
+  {
+    type = "item",
     name = "laterite-ore",
     icon = "__Sky-Atlas-Metallurgy-Rebirth__/graphics/icons/laterite-ore.png",
     icon_size = 64, icon_mipmaps = 4,
@@ -13,59 +45,136 @@ data:extend(
       { size = 64, filename = "__Sky-Atlas-Metallurgy-Rebirth__/graphics/icons/laterite-ore-3.png", scale = 0.25, mipmap_count = 4 }
     },
     subgroup = "raw-resource",
-    order = "i[laterite-ore]", -- g[uranium-ore], h[raw-fish]
+    order = "k[laterite-ore]",
+    stack_size = 50
+  },
+  {
+    type = "item",
+    name = "nickel-ore",
+    icon = "__Sky-Atlas-Metallurgy-Rebirth__/graphics/icons/nickel-ore.png",
+    icon_size = 64, icon_mipmaps = 4,
+    pictures =
+    {
+      { size = 64, filename = "__Sky-Atlas-Metallurgy-Rebirth__/graphics/icons/nickel-ore.png",   scale = 0.25, mipmap_count = 4 },
+      { size = 64, filename = "__Sky-Atlas-Metallurgy-Rebirth__/graphics/icons/nickel-ore-1.png", scale = 0.25, mipmap_count = 4 },
+      { size = 64, filename = "__Sky-Atlas-Metallurgy-Rebirth__/graphics/icons/nickel-ore-2.png", scale = 0.25, mipmap_count = 4 },
+      { size = 64, filename = "__Sky-Atlas-Metallurgy-Rebirth__/graphics/icons/nickel-ore-3.png", scale = 0.25, mipmap_count = 4 }
+    },
+    subgroup = "raw-resource",
+    order = "l[nickel-ore]",
+    stack_size = 50
+  },
+  {
+    type = "item",
+    name = "bauxite-ore",
+    icon = "__Sky-Atlas-Metallurgy-Rebirth__/graphics/icons/bauxite-ore.png",
+    icon_size = 64, icon_mipmaps = 4,
+    pictures =
+    {
+      { size = 64, filename = "__Sky-Atlas-Metallurgy-Rebirth__/graphics/icons/bauxite-ore.png",   scale = 0.25, mipmap_count = 4 },
+      { size = 64, filename = "__Sky-Atlas-Metallurgy-Rebirth__/graphics/icons/bauxite-ore-1.png", scale = 0.25, mipmap_count = 4 },
+      { size = 64, filename = "__Sky-Atlas-Metallurgy-Rebirth__/graphics/icons/bauxite-ore-2.png", scale = 0.25, mipmap_count = 4 },
+      { size = 64, filename = "__Sky-Atlas-Metallurgy-Rebirth__/graphics/icons/bauxite-ore-3.png", scale = 0.25, mipmap_count = 4 }
+    },
+    subgroup = "raw-resource",
+    order = "m[bauxite-ore]",
+    stack_size = 50
+  },
+  {
+    type = "item",
+    name = "cobaltite-ore",
+    icon = "__Sky-Atlas-Metallurgy-Rebirth__/graphics/icons/cobaltite-ore.png",
+    icon_size = 64, icon_mipmaps = 4,
+    pictures =
+    {
+      { size = 64, filename = "__Sky-Atlas-Metallurgy-Rebirth__/graphics/icons/cobaltite-ore.png",   scale = 0.25, mipmap_count = 4 },
+      { size = 64, filename = "__Sky-Atlas-Metallurgy-Rebirth__/graphics/icons/cobaltite-ore-1.png", scale = 0.25, mipmap_count = 4 },
+      { size = 64, filename = "__Sky-Atlas-Metallurgy-Rebirth__/graphics/icons/cobaltite-ore-2.png", scale = 0.25, mipmap_count = 4 },
+      { size = 64, filename = "__Sky-Atlas-Metallurgy-Rebirth__/graphics/icons/cobaltite-ore-3.png", scale = 0.25, mipmap_count = 4 }
+    },
+    subgroup = "raw-resource",
+    order = "n[cobaltite-ore]",
+    stack_size = 50
+  },
+  {
+    type = "item",
+    name = "cobalt-ore",
+    icon = "__Sky-Atlas-Metallurgy-Rebirth__/graphics/icons/cobalt-ore.png",
+    icon_size = 64, icon_mipmaps = 4,
+    pictures =
+    {
+      { size = 64, filename = "__Sky-Atlas-Metallurgy-Rebirth__/graphics/icons/cobalt-ore.png",   scale = 0.25, mipmap_count = 4 },
+      { size = 64, filename = "__Sky-Atlas-Metallurgy-Rebirth__/graphics/icons/cobalt-ore-1.png", scale = 0.25, mipmap_count = 4 },
+      { size = 64, filename = "__Sky-Atlas-Metallurgy-Rebirth__/graphics/icons/cobalt-ore-2.png", scale = 0.25, mipmap_count = 4 },
+      { size = 64, filename = "__Sky-Atlas-Metallurgy-Rebirth__/graphics/icons/cobalt-ore-3.png", scale = 0.25, mipmap_count = 4 }
+    },
+    subgroup = "raw-resource",
+    order = "o[cobalt-ore]",
     stack_size = 50
   },
   {
     type = "item",
     name = "monazite-ore",
-    icon = "__Sky-Atlas-Metallurgy-Rebirth__/graphics/icons/monazite-ore.png",
+    icon = "__base__/graphics/icons/iron-ore.png",
     icon_size = 64, icon_mipmaps = 4,
     pictures =
     {
-      { size = 64, filename = "__Sky-Atlas-Metallurgy-Rebirth__/graphics/icons/monazite-ore.png",  scale = 0.25, mipmap_count = 4}
+      { size = 64, filename = "__base__/graphics/icons/iron-ore.png",  scale = 0.25, mipmap_count = 4}
     },
     subgroup = "raw-resource",
-    order = "j[monazite-ore]",
+    order = "p[monazite-ore]",
+    stack_size = 50
+  },
+  {
+    type = "item",
+    name = "neodymium-ore",
+    icon = "__base__/graphics/icons/iron-ore.png",
+    icon_size = 64, icon_mipmaps = 4,
+    pictures =
+    {
+      { size = 64, filename = "__base__/graphics/icons/iron-ore.png",  scale = 0.25, mipmap_count = 4}
+    },
+    subgroup = "raw-resource",
+    order = "q[monazite-ore]",
     stack_size = 50
   },
   {
     type = "item",
     name = "thorium-ore",
-    icon = "__Sky-Atlas-Metallurgy-Rebirth__/graphics/icons/thorium-ore.png",
+    icon = "__base__/graphics/icons/uranium-ore.png",
     icon_size = 64, icon_mipmaps = 4,
     pictures =
     {
-      { size = 64, filename = "__Sky-Atlas-Metallurgy-Rebirth__/graphics/icons/thorium-ore.png",  scale = 0.25, mipmap_count = 4}
+      { size = 64, filename = "__base__/graphics/icons/uranium-ore.png",  scale = 0.25, mipmap_count = 4}
     },
     subgroup = "raw-resource",
-    order = "k[thorium-ore]",
+    order = "r[thorium-ore]",
     stack_size = 50
   },
   {
     type = "item",
     name = "samarium-ore",
-    icon = "__Sky-Atlas-Metallurgy-Rebirth__/graphics/icons/samarium-ore.png",
+    icon = "__base__/graphics/icons/iron-ore.png",
     icon_size = 64, icon_mipmaps = 4,
     pictures =
     {
-      { size = 64, filename = "__Sky-Atlas-Metallurgy-Rebirth__/graphics/icons/samarium-ore.png",  scale = 0.25, mipmap_count = 4}
+      { size = 64, filename = "__base__/graphics/icons/iron-ore.png",  scale = 0.25, mipmap_count = 4}
     },
     subgroup = "raw-resource",
-    order = "l[samarium-ore]",
+    order = "s[samarium-ore]",
     stack_size = 50
   },
   {
     type = "item",
     name = "uranium-233",
-    icon = "__Sky-Atlas-Metallurgy-Rebirth__/graphics/icons/uranium-233.png",
+    icon = "__base__/graphics/icons/uranium-235.png",
     icon_size = 64, icon_mipmaps = 4,
     pictures =
     {
-      { size = 64, filename = "__Sky-Atlas-Metallurgy-Rebirth__/graphics/icons/uranium-233.png",  scale = 0.25, mipmap_count = 4}
+      { size = 64, filename = "__base__/graphics/icons/uranium-235.png",  scale = 0.25, mipmap_count = 4}
     },
-    subgroup = "raw-resource",
-    order = "m[uranium-233]",
+    subgroup = "intermediate-product",
+    order = "r[uranium-233]",
     stack_size = 50
   }
 })
