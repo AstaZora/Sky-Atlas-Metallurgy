00af02fd8ffec745878811fc3c859ce17e94f162
Asta Zora <Tas_King@outlook.com>
2021-06-06 16:17:21 -0400

    Co-authored-by: Quasar-0 <Quasar-0@users.noreply.github.com>

--- prototypes/recipe/recipe.lua
+++ prototypes/recipe/recipe.lua
@@ -17,5 +17,24 @@ data:extend(
     },
     main_product = "nickel-ore",
     icon_size = 64,
+  },
+  {
+    type = "recipe",
+    name = "monazite-refining",
+    category = "centrifuging",
+    subgroup = "raw-material",
+    enabled = true,
+    energy_required = 12,
+    ingredients = {{"monazite-ore", 10}},
+    results = 
+    {
+      {"uranium-ore", 2},
+      {"thorium-ore", 2},
+      {"neodymium-ore", 2},
+      {"chromium-ore", 2},
+      {"samarium-ore", 2}
+    },
+    main_product = "uranium-ore",
+    icon_size = 64,
   }
 })
